920cdfd8e4986b1b1c4c5c0ecca3dc040368133b
hyebinpark <hoonywhitesocks@gmail.com>
2025-10-25 00:25:07 +0900

    feat(js): add examples for conditionals and loops

---
+++ 06.condition.js
@@ -0,0 +1,9 @@
+// 조건문 (condition)
+let age = 30;
+age = 12;
+
+if (age > 20) {
+  console.log('만원');
+} else {
+  console.log('오천원');
+}
---
+++ 07.loop.js
@@ -0,0 +1,11 @@
+// 반복문 (Loop)
+for (let i = 0; i < 10; i++) {
+  console.log(i); // 0부터 9까지 출력
+}
+
+let total = 0;
+for (let i = 0; i < 6; i++) {
+  total = total + i;
+}
+console.log(total); // 15
+// 1 + 2 + 3 + 4 + 5
